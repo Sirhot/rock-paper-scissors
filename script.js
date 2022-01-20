ebb5a2efe21487fffccda388922a25ccedf6e7b2
Sirhot <serhatcubukcu@outlook.com>
2022-01-20 18:20:10 +0300

    add all of them

--- script.js
+++ script.js
@@ -14,13 +14,17 @@ function whoWins(player,computer){
         [0,2,1]
     ];
 
+
+    
     //picks[player][computer]
     //what will the result be?
     //if statements and write to screen
-
+    
     if(picks[player][computer] == 0){
         //lose
-        document.getElementById("result").innerHTML = "Player LOSE!!!";
+        document.getElementById("result").innerHTML = "HOOOMAN LOST!!!";
+        compCount++;
+        document.getElementById("compCount").innerHTML = compCount;
     }
     else if(picks[player][computer] == 1){
         //draw
@@ -28,37 +32,78 @@ function whoWins(player,computer){
     }
     else if(picks[player][computer] == 2){
         //win
-        document.getElementById("result").innerHTML = "Player WON!!!";
+        document.getElementById("result").innerHTML = "HOOOMAN WON!!!";
+        playerCount++;
+        document.getElementById("playerCount").innerHTML = playerCount;
     }
-
+    
+    
 }
 
 function round(playerSelection){
+
+
     
     let computerSelection = Math.floor(Math.random()*3);
-
+    
     for(var i=0; i<3; i++){
 
+        if(playerSelection == 0){
+            //control
+            document.getElementById("player").innerHTML='<img style="width: 110px; height: 110px;" src="images/Frame 4.jpg" alt="rock"><p>Rock</p>';
+            
+        }
+        else if(playerSelection == 1){
+            //control
+            document.getElementById("player").innerHTML='<img style="width: 110px; height: 110px;" src="images/Frame 3.jpg" alt="paper"><p>Paper</p>';
+            
+        }
+        else if(playerSelection == 2){
+            //control
+            document.getElementById("player").innerHTML='<img style="width: 110px; height: 110px;" src="images/Frame 2.jpg" alt="scissors"><p>Scissors</p>';
+            
+        }
+        
         if(computerSelection == 0){
             //control
-            document.getElementById("computer").innerHTML='<img style="width: 150px; height: 150px;" src="https://media.istockphoto.com/photos/stone-pebble-gray-picture-id1288973456?b=1&k=20&m=1288973456&s=170667a&w=0&h=GBGgp4yrZv4ooDBws8yHF24sJ3rkEpObYsBWpVNKFT8=" alt="rock"><p>Rock</p>';
-             
+            document.getElementById("computer").innerHTML='<img style="width: 110px; height: 110px;" src="images/Frame 4.jpg" alt="rock"><p>Rock</p>';
+            
         }
         else if(computerSelection == 1){
             //control
-            document.getElementById("computer").innerHTML='<img style="width: 150px; height: 150px;" src="https://img.freepik.com/free-photo/design-space-paper-textured-background_53876-42312.jpg?size=626&ext=jpg" alt="paper"><p>Paper</p>';
-             
+            document.getElementById("computer").innerHTML='<img style="width: 110px; height: 110px;" src="images/Frame 3.jpg" alt="paper"><p>Paper</p>';
+            
         }
         else if(computerSelection == 2){
             //control
-            document.getElementById("computer").innerHTML='<img style="width: 150px; height: 150px;" src="https://upload.wikimedia.org/wikipedia/commons/7/76/Pair_of_scissors_with_black_handle%2C_2015-06-07.jpg" alt="scissors"><p>Scissors</p>';
-             
+            document.getElementById("computer").innerHTML='<img style="width: 110px; height: 110px;" src="images/Frame 2.jpg" alt="scissors"><p>Scissors</p>';
+            
         }
         
     }
     
     whoWins(playerSelection,computerSelection);
-
+    
+    //result
+    if(compCount == 5){
+    
+        document.getElementById("result").innerHTML = "COMPUTER WON THE GAME";
+        
+        compCount = 0;
+        playerCount = 0;
+        document.getElementById("compCount").innerHTML = compCount;
+        document.getElementById("playerCount").innerHTML = playerCount;
+    }
+    if(playerCount == 5){
+        
+        document.getElementById("result").innerHTML = "HOOOMANN WON THE GAME";
+        
+        compCount = 0;
+        playerCount = 0;
+        document.getElementById("compCount").innerHTML = compCount;
+        document.getElementById("playerCount").innerHTML = playerCount;
+    }
 }
 
-
+var playerCount = 0;
+var compCount = 0;
